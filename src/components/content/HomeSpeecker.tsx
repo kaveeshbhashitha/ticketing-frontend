8b97c275fa54464be67c7c210c00083379e06e86
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2025-01-04 09:25:15 +0530

    update Homespeeker component to display 6 events

--- src/components/content/HomeSpeecker.tsx
+++ src/components/content/HomeSpeecker.tsx
@@ -4,27 +4,25 @@ import { Event } from "../../interfaces/Event";
 import "../../styles/HomeSpeecker.css";
 
 const HomeSpeecker: React.FC = () => {
-
   const [events, setEvents] = useState<Event[]>([]);
-  
-    useEffect(() => {
-      const fetchEvents = async () => {
-        try {
-          const eventList = await getAllOtherEventDataForFrontEnd();
-          if (eventList && eventList.length > 0) {
-            setEvents(eventList);
-            console.error(""); 
-          } else {
-            console.error("No events found to display.");
-          }
-        } catch (error) {
-          console.error("Error fetching event data.");
-          console.error("Failed to fetch events:", error);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const eventList = await getAllOtherEventDataForFrontEnd();
+        if (eventList && eventList.length > 0) {
+          setEvents(eventList);
+          console.error("");
+        } else {
+          console.error("No events found to display.");
         }
-      };
-  
-      fetchEvents();
-    }, []);
+      } catch (error) {
+        console.error("Failed to fetch events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
   return (
     <section id="speakers" className="wow fadeInUp">
       <div className="container">
@@ -37,24 +35,38 @@ const HomeSpeecker: React.FC = () => {
           {events.map((event) => (
             <div className="col-lg-4 col-md-6" key={event.eventId}>
               <div className="speaker">
-                <img src={`data:${event.contentType};base64,${event.imageData}`} alt={event.eventName} className="img-fluid" style={{height:"280px", width:"100%"}}/>
+                <img
+                  src={`data:${event.contentType};base64,${event.imageData}`}
+                  alt={event.eventName}
+                  className="img-fluid"
+                  style={{ height: "280px", width: "100%" }}
+                />
                 <div className="details">
                   <h3>
-                  <a href="/" title={event.eventName}>{event.eventName.length > 20
-                      ? `${event.eventName.substring(0, 26)}...`
-                      : event.eventName}</a>
+                    <a href="/" title={event.eventName}>
+                      {event.eventName.length > 20
+                        ? `${event.eventName.substring(0, 26)}...`
+                        : event.eventName}
+                    </a>
                   </h3>
                   <span className="test">{event.eventDate}</span>
-                  <span className="test">{event.startTime} to {event.endTime}</span>
-                  <span className="test"><br />
+                  <br />
+                  <span className="test">
+                    {event.startTime} to {event.endTime}
+                  </span>
+                  <span className="test">
+                    <br />
                     <i className="fa-solid fa-location-dot rightgap"></i> At{" "}
                     {event.eventVenue}
-                  </span><br />
-                  <span className="test">{event.oneTicketPrice}.00 LKR upwards</span>
+                  </span>
+                  <br />
+                  <span className="test">
+                    {event.oneTicketPrice}.00 LKR upwards
+                  </span>
                   <div className="social">
-                      <a href="/">
-                        <i className="fa-solid fa-cart-shopping">&nbsp;Buy</i>
-                      </a>
+                    <a href="/">
+                      <i className="fa-solid fa-cart-shopping">&nbsp;Buy</i>
+                    </a>
                   </div>
                 </div>
               </div>
