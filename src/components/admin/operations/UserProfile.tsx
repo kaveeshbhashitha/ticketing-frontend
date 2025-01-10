543599a58cde615e3bf7c8aa1c84aa2aa19b410b
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2025-01-11 00:05:39 +0530

    Add chart.js and react-chartjs-2 dependencies; update MyTickets component to use deleteReservation and implement getEventsByUserId service method in EventService.

--- src/components/admin/operations/UserProfile.tsx
+++ src/components/admin/operations/UserProfile.tsx
@@ -1,11 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../layout/Header";
 import EventAbout from "../../content/Events/EventAbout";
 import Footer from "../../layout/Footer";
 import Chatbot from "../../chatbot/Chatbot";
+import useAuthCheck from "../../../useAuthCheck";
+import { User } from "../../../interfaces/User";
+import { useNavigate } from "react-router-dom";
+import { deleteUser, getUserByEmail, updateUser } from "../../../service/UserService";
+import { logout } from "../../../service/AuthService";
+import { Event } from "../../../interfaces/Event";
+import { getEventsByUserId } from "../../../service/EventService";
 
 const UserProfile: React.FC = () => {
-  
+  useAuthCheck(["User", "Admin"]);
+  const [userData, setUserData] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [, setError] = useState<string | null>(null);
+  const [events, setEvents] = useState<Event[] | null>(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userEmail = sessionStorage.getItem("user"); 
+        if (!userEmail) throw new Error("User email not found");
+
+        const data = await getUserByEmail(userEmail);
+        const event = await getEventsByUserId(data.userId);
+
+        setUserData(data);
+        setEvents(event);
+        console.log(events);
+
+      } catch {
+        setError("Failed to fetch user data. Please try again later.");
+
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleUpdate = async () => {
+    if (!userData) return;
+    try {
+      await updateUser(userData.userId, userData);
+      logout();
+      navigate("/");
+    } catch {
+      setError("Failed to update user profile. Please try again.");
+    }
+  };
+
+  const handleDelete = async () => {
+    if (!userData) return;
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete your account? This action cannot be undone."
+    );
+    if (confirmDelete) {
+      try {
+        await deleteUser(userData.userId);
+        logout();
+        navigate("/");
+      } catch{
+        setError("Failed to delete account. Please try again later.");
+      }
+    }
+  };
+
+  if (loading) return <p>Loading user data...</p>;
 
   return (
     <div>
@@ -20,37 +86,26 @@ const UserProfile: React.FC = () => {
                     <div className="d-flex flex-column align-items-center text-center">
                       <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
                       <div className="mt-3">
-                        <h4>John Doe</h4>
-                        <p className="text-secondary mb-1">Full Stack Developer</p>
-                        <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                        <button className="btn btn-primary">Follow</button>
-                        <button className="btn btn-outline-primary">Message</button>
+                        <h4>{`${userData?.firstName} ${userData?.lastName}`}</h4>
+                        <p className="text-muted font-size-sm">{`${userData?.userEmail} | ${userData?.userRole}`}</p>
+                        <button className="btn btn-outline-danger" onClick={handleDelete}>Delete My Account</button>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="card mt-3">
+                  <h5 className="btn btn-outline-primary m-2">My Recent Activities</h5>
                   <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                      <span className="text-secondary">https://bootdey.com</span>
+                    {events?.map((event) => (
+                    <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap" key={event.eventId}>
+                      <h6 className="mb-0"><img
+                        src={`data:${event.contentType};base64,${event.imageData}`}
+                        alt="Event"
+                        className="tableimg"
+                      /> <span className="mx-3">{event.eventType}</span></h6>
+                      <span className="text-secondary"><a href={`http://localhost:5173/AllEvents`}>Go to event</a></span>
                     </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                      <span className="text-secondary">bootdey</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
-                      <span className="text-secondary">@bootdey</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                      <span className="text-secondary">bootdey</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                      <span className="text-secondary">bootdey</span>
-                    </li>
+                    ))}
                   </ul>
                 </div>
               </div>
@@ -59,52 +114,42 @@ const UserProfile: React.FC = () => {
                   <div className="card-body">
                     <div className="row">
                       <div className="col-sm-3">
-                        <h6 className="mb-0">Full Name</h6>
+                        <h6 className="mb-0">First Name</h6>
                       </div>
-                      <div className="col-sm-9 text-secondary">
-                        Kenneth Valdez
+                      <input className="col-sm-9 text-secondary border-0" value={userData?.firstName}/>
+                    </div>
+                    <hr />
+                    <div className="row">
+                      <div className="col-sm-3">
+                        <h6 className="mb-0">Last Name</h6>
                       </div>
+                      <input className="col-sm-9 text-secondary border-0" value={userData?.lastName}/>
                     </div>
                     <hr />
                     <div className="row">
                       <div className="col-sm-3">
                         <h6 className="mb-0">Email</h6>
                       </div>
-                      <div className="col-sm-9 text-secondary">
-                        fip@jukmuh.al
-                      </div>
+                      <input className="col-sm-9 text-secondary border-0" value={userData?.userEmail}/>
                     </div>
                     <hr />
                     <div className="row">
                       <div className="col-sm-3">
-                        <h6 className="mb-0">Phone</h6>
+                        <h6 className="mb-0">User Role</h6>
                       </div>
-                      <div className="col-sm-9 text-secondary">
-                        (239) 816-9029
-                      </div>
+                      <input className="col-sm-9 text-secondary border-0" value={userData?.userRole} disabled/>
                     </div>
                     <hr />
                     <div className="row">
                       <div className="col-sm-3">
-                        <h6 className="mb-0">Mobile</h6>
+                        <h6 className="mb-0">Timestamp</h6>
                       </div>
-                      <div className="col-sm-9 text-secondary">
-                        (320) 380-4539
-                      </div>
-                    </div>
-                    <hr />
-                    <div className="row">
-                      <div className="col-sm-3">
-                        <h6 className="mb-0">Address</h6>
-                      </div>
-                      <div className="col-sm-9 text-secondary">
-                        Bay Area, San Francisco, CA
-                      </div>
+                      <input className="col-sm-9 text-secondary border-0" value={`You have Registered on: ${userData?.dateRegistered} and at: ${userData?.timeRegistered}`} disabled/>
                     </div>
                     <hr />
                     <div className="row">
                       <div className="col-sm-12">
-                        <a className="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
+                        <button className="btn btn-outline-dark" onClick={handleUpdate}>Edit My Profile</button>
                       </div>
                     </div>
                   </div>
