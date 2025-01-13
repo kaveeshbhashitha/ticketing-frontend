8259d4aebad7b291b3b2db99bdfb0d5dafb6ab28
PasinduPramudithaJ <pramudithapasindu48@gmail.com>
2025-01-13 13:21:55 +0530

    Add the Event Description into All event categories

--- src/components/content/Events/EventDisplayOther.tsx
+++ src/components/content/Events/EventDisplayOther.tsx
@@ -5,27 +5,26 @@ import "../../../styles/HomeSpeecker.css";
 import { Link } from "react-router-dom";
 
 const EventDisplayOther: React.FC = () => {
-
   const [events, setEvents] = useState<Event[]>([]);
-  
-    useEffect(() => {
-      const fetchEvents = async () => {
-        try {
-          const eventList = await getAllOtherEvents();
-          if (eventList && eventList.length > 0) {
-            setEvents(eventList);
-            console.error(""); 
-          } else {
-            console.error("No events found to display.");
-          }
-        } catch (error) {
-          console.error("Error fetching event data.");
-          console.error("Failed to fetch events:", error);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const eventList = await getAllOtherEvents();
+        if (eventList && eventList.length > 0) {
+          setEvents(eventList);
+          console.error("");
+        } else {
+          console.error("No events found to display.");
         }
-      };
-  
-      fetchEvents();
-    }, []);
+      } catch (error) {
+        console.error("Error fetching event data.");
+        console.error("Failed to fetch events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
   return (
     <section id="speakers" className="wow fadeInUp">
       <div className="container">
@@ -38,24 +37,43 @@ const EventDisplayOther: React.FC = () => {
           {events.map((event) => (
             <div className="col-lg-4 col-md-6" key={event.eventId}>
               <div className="speaker">
-                <img src={`data:${event.contentType};base64,${event.imageData}`} alt={event.eventName} className="img-fluid" style={{height:"280px", width:"100%"}}/>
+                <img
+                  src={`data:${event.contentType};base64,${event.imageData}`}
+                  alt={event.eventName}
+                  className="img-fluid"
+                  style={{ height: "280px", width: "100%" }}
+                />
                 <div className="details">
                   <h3>
-                  <a href="/" title={event.eventName}>{event.eventName.length > 20
-                      ? `${event.eventName.substring(0, 26)}...`
-                      : event.eventName}</a>
+                    {/* Use Link component to navigate to the event description page */}
+                    <Link
+                      to={`/event/${event.eventId}`}
+                      title={event.eventName}
+                    >
+                      {event.eventName.length > 20
+                        ? `${event.eventName.substring(0, 26)}...`
+                        : event.eventName}
+                    </Link>
                   </h3>
                   <span className="test">{event.eventDate}</span> <br />
-                  <span className="test">{event.startTime} to {event.endTime}</span>
-                  <span className="test"><br />
+                  <span className="test">
+                    {event.startTime} to {event.endTime}
+                  </span>
+                  <span className="test">
+                    <br />
                     <i className="fa-solid fa-location-dot rightgap"></i> At{" "}
                     {event.eventVenue}
-                  </span><br />
-                  <span className="test">{event.oneTicketPrice}.00 LKR upwards</span>
+                  </span>
+                  <br />
+                  <span className="test">
+                    {event.oneTicketPrice}.00 LKR upwards
+                  </span>
                   <div className="social">
-                      <Link to={`/reservation/${event.eventId}`}>
-                        <i className="fa-solid fa-cart-shopping">&nbsp;Buy Tickets</i>
-                      </Link>
+                    <Link to={`/reservation/${event.eventId}`}>
+                      <i className="fa-solid fa-cart-shopping">
+                        &nbsp;Buy Tickets
+                      </i>
+                    </Link>
                   </div>
                 </div>
               </div>
