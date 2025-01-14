e6717de287d3ac4d460961a0f15f7fd6b9f97569
PasinduPramudithaJ <pramudithapasindu48@gmail.com>
2025-01-14 20:19:07 +0530

    Add Owl.carosoul in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 import './index.css'
 import App from './App.tsx'
 
