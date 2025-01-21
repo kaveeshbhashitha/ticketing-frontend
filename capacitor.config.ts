43b095731376586985874752a778c73fcf7dbebf
PasinduPramudithaJ <pramudithapasindu48@gmail.com>
2025-01-21 13:25:14 +0530

    Configure the android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'ticketing-frontend',
+  appName: 'TheEvent',
   webDir: 'dist'
 };
 
